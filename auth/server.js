1775963218b9fb48bed1b3efe72951742f96a9a5
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2024-09-23 11:21:55 +0530

    Update server.js

--- auth/server.js
+++ auth/server.js
@@ -22,6 +22,6 @@ app.post("/auth", function (req, res) {
   res.status(403).send();
 });
 
-app.listen(8080, function () {
-  console.log("Listening on port 8080!");
+app.listen(8000, function () {
+  console.log("Listening on port 8000!");
 });
